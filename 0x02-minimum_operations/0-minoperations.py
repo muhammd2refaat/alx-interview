36b0064029b721f408d44f99b0b06e1fc78188d5
muhammd2refaat <mohammed_refat@inbox.ru>
2024-10-20 22:08:09 +0300

    Refactor minOperations function and add docstring

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,6 +3,9 @@
 Calculates the fewest number of operations needed to result in exactly n H characters in the file.
 """
 def minOperations(n):
+    '''
+    Method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    '''
     if n <= 1:
         return 0
     for i in range(2, n + 1):
