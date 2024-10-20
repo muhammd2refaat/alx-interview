7c7fc0de213aa5ac9a22a9338494925987019f74
muhammd2refaat <mohammed_refat@inbox.ru>
2024-10-20 22:04:31 +0300

    Add solution for finding the minimum operations
    This commit adds a solution for finding the minimum operations required to reach a given number 'n'. The code implements a recursive function 'minOperations' that calculates the minimum number of operations needed to reach 'n' by either doubling the current value or incrementing it by 1. The function returns the minimum number of operations required.

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
