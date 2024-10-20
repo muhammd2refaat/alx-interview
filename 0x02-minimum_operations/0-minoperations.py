1e14afdaa9d289d0a7c80e36d68848143fbfe249
muhammd2refaat <mohammed_refat@inbox.ru>
2024-10-20 22:06:12 +0300

    Add docstring explaining the purpose of the minOperations function

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+"""
 def minOperations(n):
     if n <= 1:
         return 0
