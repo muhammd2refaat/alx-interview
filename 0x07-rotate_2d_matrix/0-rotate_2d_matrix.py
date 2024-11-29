bebde5c2d4e55cafbd24a3c69848c26f6c2ca0ce
muhammd2refaat <mohammed_refat@inbox.ru>
2024-11-29 22:18:19 +0200

    Add function to rotate a 2D matrix in place

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""2D matrix rotation module.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates an m by n 2D matrix in place.
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
